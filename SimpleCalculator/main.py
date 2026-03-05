23a4d3558f50056622784c450a819063e75d078a
Sujay Muramalla <sujay.muramalla@gmail.com>
2026-03-05 16:50:54 +0100

    added SimpleCalculator project

---
+++ SimpleCalculator/main.py
@@ -0,0 +1,58 @@
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a*b
+
+def divide(a, b):
+    return a/b
+
+
+
+choice = int(input("Enter your choice: "))
+
+while True:
+
+    print("\nCalculator Menu")
+    print("1 Add")
+    print("2 Subtract")
+    print("3 Multiply")
+    print("4 Divide")
+    print("5 Exit")
+
+    choice = int(input("Enter choice: "))
+
+    if choice == 5:
+        print("Exit...")
+        break
+
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+
+    match choice:
+
+        case 1:
+            print(a + b)
+
+        case 2:
+            print(a - b)
+
+        case 3:
+            print(a * b)
+
+        case 4:
+            print(a / b)
+
+        case _:
+            print("Invalid option")
+    
+        
+
+
+
+
+
