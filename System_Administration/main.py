1515ef9b20496ff1a123d9f57819040f3f659113
Sujay Muramalla <sujay.muramalla@gmail.com>
2026-03-06 16:58:15 +0100

    checks if the user exists using ls -la /home/<username> and creates if not

--- System_Administration/main.py
+++ System_Administration/main.py
@@ -5,22 +5,32 @@ import subprocess
 pem_file = "/c/Users/Lenovo/OneDrive/Backup_23102024/d_drive/PERSONAL/Career/Projects/Beginner_Projects/labsuser.pem"
 ec2_user = "ec2-user"
 ec2_public_ip = "54.188.162.118"
-new_user = "sujay1"
+new_user = "sujay2"
+remote_command = f"ls -la /home/{new_user}"
 
 
+#check if the user exists, 
 check_user = subprocess.run([
     "ssh",
     "-i", pem_file,
     f"{ec2_user}@{ec2_public_ip}",
-    f"id {new_user}"
+    remote_command
 ])
 
-subprocess.run ([
-    "ssh",
-    "-i", pem_file,
-    f"{ec2_user}@{ec2_public_ip}",
-    f"sudo useradd -m {new_user}"
-])
+#if returncode is not 0, creates the user
+if check_user.returncode!=0:
+    print ("user does not exist! you can add a new user")
+    subprocess.run ([
+        "ssh",
+        "-i", pem_file,
+        f"{ec2_user}@{ec2_public_ip}",
+        f"sudo useradd -m {new_user}"
+    ])
+else:
+    print ("user already exists")
+
+
+
 
 
 
