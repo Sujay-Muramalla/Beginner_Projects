6d01338fa42b93abc83cae91cbca4eb3c80a0f3e
Sujay Muramalla <sujay.muramalla@gmail.com>
2026-03-10 11:07:16 +0100

    code not working

--- Prime_Number/main.py
+++ Prime_Number/main.py
@@ -3,23 +3,7 @@ max = 250
 nums = list (range(1,max+1))
 print (nums)
 
-""" primes = []
-divisor=2
-for num in nums:
-    print (num)
-    isPrime = True 
-    if (num>2):
-        if (num%divisor==0):
-            isPrime = False
-            break
-        else:
-            divisor+=1
-            
-    else:
-        primes.append(num)
-    
-
-print (primes) """
+primes = []
 divisor = 2
 for num in nums:
     if num  < 2:
@@ -34,3 +18,6 @@ for num in nums:
     
     if isPrime == True:
         primes.append(num)
+        
+
+print (primes)
