7fe073728560bab9475762c3aa11a43834f54028
Sujay Muramalla <sujay.muramalla@gmail.com>
2026-03-10 11:36:43 +0100

    added logic for user input and try catch block to avoid invalid inputs

--- Prime_Number/main.py
+++ Prime_Number/main.py
@@ -1,25 +1,36 @@
 
-max = 250
+
+
+try:
+    
+    max = int (input ("Enter the max number for generating primes: "))
+    #print (max)
+
+except ValueError:
+    print ("Invalid input! Please enter a valid integer")
+
+#max = 250
 nums = list (range(1,max+1))
-print (nums)
+#print (nums)
 
 primes = []
 divisor = 2
-isPrime = True
+#isPrime = True
 
 for num in nums:
+    isPrime = True
     if num < 2:
         isPrime = False 
     else:
         for divisor in range(2,num-1):
             if num%divisor == 0:
                 isPrime = False
-                print (f"{num} is not a prime")
+                #print (f"{num} is not a prime")
                 break
     if (isPrime == True):
         primes.append(num)
         
 
-print (primes)
+print (f"Prime numbers are: {primes}")
 
 
